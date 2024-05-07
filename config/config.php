ab820b9e14e8ea747b238d69bf58f798ae2d0d15
Waleed Ashraf <waleedashraf9t@gmail.com>
2024-05-07 20:32:30 +0500

    add assets, config.php to git

---
+++ config/config.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    /*'database' => [
+        'host' => 'localhost',
+        'port' => '3306',
+        'dbname' => 'bdev',
+        'charset' => 'utf8mb4'
+    ],*/
+    'routes' => [
+        '/' => '/index.php',
+        '/home'=> '/index.php',
+        '/about'=> '/about.php',
+        '/contact'=> '/contact.php',
+        '/work'=> '/work.php',
+        '/404'=> '/404.php',
+    ]
+];
